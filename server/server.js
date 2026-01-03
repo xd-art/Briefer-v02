6e4894d2bc2b2d1723ae9cb22985748bb7b786ef
Danylo Khorzhevskyi <dkhorz@hotmail.com>
2026-01-02 16:26:28 -0800

    Update route handling for React app

--- server/server.js
+++ server/server.js
@@ -50,7 +50,8 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../build')));
 
     // Handle React routing, return all requests to React app
-    app.get('*', (req, res) => {
+        app.get(/(.*)/, (req, res) => {
+
         res.sendFile(path.join(__dirname, '../build', 'index.html'));
     });
 }
